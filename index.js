4d7edf87a7b4a5a9a113bab4e2f00cbd37631720
Anastasia Fadeeva <anfd.design@gmail.com>
2023-02-12 23:36:52 +0100

    changes for cors

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const mongoose = require('mongoose')
 const router = require('./router/index')
 const errorMiddleware = require('./middlewares/error-middleware')
 
+const allowedOrigins = [process.env.CLIENT_URL, process.env.LOCAL_URL]
+const corsOptions = {
+   origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+         callback(null, true)
+      } else {
+         callback(new Error('Not allowed by CORS'))
+      }
+   },
+   optionsSuccessStatus: 200,
+}
+
 const PORT = process.env.PORT || 3001
 const app = express()
 
 // Middlewares
 app.use(express.json())
 app.use(cookieParser())
-// Обязательно указать с каким доменом обмениваться данными
-app.use(
-   cors({
-      credentials: true,
-      origin: process.env.CLIENT_URL,
-      allowedHeaders: ['Authorization'],
-   }),
-)
+
+app.use(cors(corsOptions))
 
 app.use('/api', router)
 // Error middleware должен идти последним
